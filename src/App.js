a23142ec14be750256d42cb655ac44c88906f546
Martin Papa <martin.papa@outlook.com>
2021-07-15 16:38:15 -0700

    Installed Semantic UI

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+function App() {
+  return(
+    <div>
+    <Button primary>Primary</Button>
+    <Button secondary>Secondary</Button>
+  </div>
+  )
+}
+export default App
